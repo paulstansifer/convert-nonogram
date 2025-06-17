744b590e1bbd7eb9481c3059d62752658b6dffaf
Paul Stansifer <paul.stansifer@gmail.com>
2025-06-17 09:08:17 -0400

    Okay, someone out there figured out how to make yield work on the Web!

--- src/gui.rs
+++ src/gui.rs
@@ -77,12 +77,15 @@ pub async fn yield_now() {
 
 #[cfg(target_arch = "wasm32")]
 pub async fn yield_now() {
-    //surely there's a better way...
-    wasm_bindgen_futures::JsFuture::from(js_sys::Promise::resolve(
-        &wasm_bindgen::JsValue::UNDEFINED,
-    ))
-    .await
-    .unwrap();
+    // Taken from https://github.com/rustwasm/wasm-bindgen/issues/3359:
+    let mut cb = |resolve: js_sys::Function, _reject: js_sys::Function| {
+        web_sys::window()
+            .unwrap()
+            .set_timeout_with_callback_and_timeout_and_arguments_0(&resolve, 1)
+            .expect("Failed to call set_timeout");
+    };
+    let p = js_sys::Promise::new(&mut cb);
+    wasm_bindgen_futures::JsFuture::from(p).await.unwrap();
 }
 
 struct NonogramGui {
